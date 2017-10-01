d5470c26a2fd7700cd5def0096c6141a4548a1f3
Nakhon41 <jo_sometime@hotmail.com>
2017-10-02 00:44:59 +0700

    Crate program A

---
+++ src/a/A.java
@@ -0,0 +1,24 @@
+package a;
+
+public class A {
+	double sarary;
+	double sale;
+	double result =0;
+	
+	public A(double salary, double com) {
+		this.sarary=salary;
+		this.sale =com;
+	}
+	public double find() {
+		if(this.sale>50000) {
+			this.result=(this.sale*(0.03))+this.sarary;
+		}else if(this.sale>25000) {
+			this.result=(this.sale*(0.02))+this.sarary;
+		}else
+		{
+			this.result=(this.sale*(0.01))+this.sarary;
+		}
+		
+		return this.result;
+	}
+}
